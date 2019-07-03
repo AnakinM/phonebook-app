e8213a96f52883d748fddd34722137aa9bd79343
AnakinM <mateusz.anikiej@gmail.com>
2019-07-03 22:38:32 +0100

    Created templates and added listing all contacts

--- phone_book/contacts/views.py
+++ phone_book/contacts/views.py
@@ -1,13 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Person
 
 # Show all contacts
 def contact_list_view(request):
-    return HttpResponse("<h1>Contact List View</h1>")
+    # What I did here is that I put myself a challenge to fit in, suggested in task, database models.
+    # Normally I couldn't access Phone and Email fields from Person object, as
+    # foreign key of Person is in both Phone and Email, not the other way (Phone and Email as
+    # foreign keys being in Person). So I followed relationship "backward" like you can see down here.
+    # As list of contacts is small, this loop don't steal much execution time. Yet, I'd take a different 
+    # approach in designing database models to avoid making such loops.
+
+    contacts = Person.objects.all() 
+    for contact in contacts:
+        contact.phone = contact.phone_set.get(person=contact).phone
+        contact.email = contact.email_set.get(person=contact).email
+    
+    template = "contacts/list.html"
+    context = {"contacts": contacts}
+    return render(request, template, context)
 
 # Show specific contact
 def contact_detail_view(request, id):
-    return HttpResponse("<h1>Contact Detail View</h1>")
+    contact = Person.objects.get(pk=id)
+    contact.phone = contact.phone_set.get(person=contact).phone
+    contact.email = contact.email_set.get(person=contact).email
+    template = "contact/details.html"
+    context = {"contact": contact}
+    return render(request, template, context)
 
 # Add new contact
 def contact_add_view(request):
